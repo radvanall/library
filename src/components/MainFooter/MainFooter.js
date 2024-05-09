d9e4e373aaed3855691ba36b6b9db11f3e457016
Radvan Alexandru <rip777ton@gmail.com>
2024-05-09 21:18:32 +0300

    Create the MainFooter page.

---
+++ src/components/MainFooter/MainFooter.js
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import { MainContext } from "../../context/MainContext";
+import Pagination from "../Pagination/Pagination";
+import usePagination from "../../hooks/usePagination";
+
+const MainFooter = () => {
+  const { params, setParams, books } = useContext(MainContext);
+  const {
+    pages,
+    currentPage,
+    changePage,
+    toFirstPage,
+    toLastPage,
+    toNextPage,
+    toPreviousPage,
+  } = usePagination(params, setParams, books?.totalNumber || 5, 5);
+
+  return (
+    <div>
+      {books && (
+        <Pagination
+          pages={pages}
+          currentPage={currentPage}
+          next={books?.next || null}
+          previous={books?.previous || null}
+          changePage={changePage}
+          toFirstPage={toFirstPage}
+          toLastPage={toLastPage}
+          toNextPage={toNextPage}
+          toPreviousPage={toPreviousPage}
+        />
+      )}
+    </div>
+  );
+};
+
+export default MainFooter;
