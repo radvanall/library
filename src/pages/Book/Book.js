4f6db117f9a5b4510af688d7372b68418375b553
Radvan Alexandru <rip777ton@gmail.com>
2024-05-10 21:04:56 +0300

    Add BookData and BookComments components on the Book page.

--- src/pages/Book/Book.js
+++ src/pages/Book/Book.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import BookData from "../../components/BookData/BookData";
+import BookComments from "../../components/BookComments/BookComments";
 import useGet from "../../api/useGet";
 
 const Book = () => {
   const { id } = useParams();
   const { data, loading, error } = useGet(`books/${id}`);
   console.log("book", data);
-  return <div>{data && <BookData book={data} />}</div>;
+  return (
+    <div>
+      {data && (
+        <>
+          <BookData book={data} />
+          <BookComments id={id} />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Book;
