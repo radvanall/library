e477ee5f3fd8d6e85eeb4f211372b963c9d2bca4
Radvan Alexandru <rip777ton@gmail.com>
2024-05-10 21:03:23 +0300

    Move the handleGenreChange handler to the MainContext.

--- src/components/GenreFilter/GenreFilter.js
+++ src/components/GenreFilter/GenreFilter.js
@@ -1,29 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import GenreBox from "../../components/GenreBox/GenreBox";
 import styles from "./GenreFilter.module.scss";
+import { MainContext } from "../../context/MainContext";
 // import useGet from "../../api/useGet";
-const GenreFilter = ({ data, isLoading, error }) => {
+// const GenreFilter = ({ data, isLoading, error }) => {
+const GenreFilter = () => {
   //   const { data, error, isLoading } = useGet("genres/book-count");
-  const handleGenreChange = (e) => {
-    console.log(e.target.checked);
-    // e.target.checked = !e.target.checked;
-    console.log(e.target.name);
-    console.log(e.target.id);
-    console.log(e.target);
-  };
+  const { genres, isLoading, error, params, setParams, handleGenreChange } =
+    useContext(MainContext);
+  // const handleGenreChange = (e) => {
+  //   console.log(e.target.checked);
+  //   // e.target.checked = !e.target.checked;
+  //   console.log(e.target.name);
+  //   console.log(e.target.id);
+  //   console.log(e.target);
+  //   let newIgnoredGenres = JSON.parse(params.ignoredGenres);
+  //   console.log(newIgnoredGenres);
+
+  //   if (e.target.checked) {
+  //     const index = newIgnoredGenres.indexOf(parseInt(e.target.id));
+  //     console.log("index=", index);
+  //     if (index >= 0) {
+  //       newIgnoredGenres.splice(index, 1);
+  //     }
+  //     console.log("newIgnoredGenres slice=", newIgnoredGenres);
+  //   } else {
+  //     newIgnoredGenres.push(parseInt(e.target.id));
+  //   }
+  //   console.log(newIgnoredGenres);
+  //   setParams({
+  //     ...params,
+  //     page: 1,
+  //     ignoredGenres: JSON.stringify(newIgnoredGenres),
+  //   });
+  // };
+
+  console.log("data", genres);
   return (
     <div className={styles.genre__filter__container}>
       {isLoading && <p>Is loading</p>}
       {error && <p>Error</p>}
-      {data &&
-        data
+      {genres &&
+        genres
           .filter((item) => item.nrOfBooks !== 0)
           .map((genre) => (
             <GenreBox
               label={genre.genre}
               key={genre.id}
               id={genre.id}
-              defaultChecked="true"
+              defaultChecked={
+                params?.requiredGenre
+                  ? parseInt(params.requiredGenre) === parseInt(genre.id)
+                    ? true
+                    : false
+                  : true
+              }
               quantity={genre.nrOfBooks}
               onChange={handleGenreChange}
               name={genre.genre}
--- src/context/MainContext.js
+++ src/context/MainContext.js
@@ -1,4 +1,5 @@
-import { useContext, createContext, useState, useEffect } from "react";
+import { useContext, createContext, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import useGet from "../api/useGet";
 // const params = {
 //   page: 1,
@@ -8,9 +9,18 @@ import useGet from "../api/useGet";
 const MainContext = createContext({});
 
 const MainProvider = ({ children }) => {
+  const [searchParams] = useSearchParams();
+  let requiredGenre = null;
+  if (searchParams.has("genreId")) {
+    requiredGenre = searchParams.get("genreId");
+    searchParams.delete("genreId");
+  }
+
+  console.log("requiredGenre=", requiredGenre);
   const [params, setParams] = useState({
     page: 1,
-    limit: 10,
+    limit: 3,
+    requiredGenre,
     ignoredGenres: JSON.stringify([]),
   });
   const { data: genres, error, isLoading } = useGet("genres/book-count");
@@ -19,6 +29,41 @@ const MainProvider = ({ children }) => {
     error: booksError,
     isLoading: booksAreLoading,
   } = useGet("books", params, [params]);
+
+  const handleGenreChange = (e) => {
+    console.log(e.target.checked);
+    // e.target.checked = !e.target.checked;
+    console.log(e.target.name);
+    console.log(e.target.id);
+    console.log(e.target);
+    let newIgnoredGenres = JSON.parse(params.ignoredGenres);
+    console.log(newIgnoredGenres);
+
+    if (e.target.checked) {
+      if (params?.requiredGenre) {
+        newIgnoredGenres = genres
+          .filter(
+            (genre) => parseInt(genre.id) !== parseInt(params.requiredGenre)
+          )
+          .map((genre) => parseInt(genre.id));
+      }
+      const index = newIgnoredGenres.indexOf(parseInt(e.target.id));
+      console.log("index=", index);
+      if (index >= 0) {
+        newIgnoredGenres.splice(index, 1);
+      }
+      console.log("newIgnoredGenres slice=", newIgnoredGenres);
+    } else {
+      newIgnoredGenres.push(parseInt(e.target.id));
+    }
+    console.log(newIgnoredGenres);
+    setParams({
+      ...params,
+      requiredGenre: null,
+      page: 1,
+      ignoredGenres: JSON.stringify(newIgnoredGenres),
+    });
+  };
 
   return (
     <MainContext.Provider
@@ -31,6 +76,7 @@ const MainProvider = ({ children }) => {
         booksAreLoading,
         params,
         setParams,
+        handleGenreChange,
       }}
     >
       {children}
