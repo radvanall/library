0ae1a46318cdff6e78f0970b5082e561abd7a4cb
Radvan Alexandru <rip777ton@gmail.com>
2024-05-10 21:06:41 +0300

    Add MainProvider, MainFooter and BookContainer components on the Main page.

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,24 +1,27 @@
 import React from "react";
-
+import { useContext } from "react";
+import { MainContext, MainProvider } from "../../context/MainContext";
+import BooksContainer from "../../components/BooksContainer/BooksContainer";
+import MainFooter from "../../components/MainFooter/MainFooter";
 import GenreFilter from "../../components/GenreFilter/GenreFilter";
 import useGet from "../../api/useGet";
 const params = {
   page: 1,
   limit: 10,
-  ignoredGenres: [1],
+  ignoredGenres: JSON.stringify([1, 7, 13]),
 };
+
 const Main = () => {
-  const { data, error, isLoading } = useGet("genres/book-count");
-  const {
-    books,
-    error: booksError,
-    isLoading: booksAreLoading,
-  } = useGet("/books", params);
   return (
-    <div style={{ padding: "10px", boxSizing: "border-box" }}>
-      Main
-      <GenreFilter data={data} error={error} isLoading={isLoading} />
-    </div>
+    <MainProvider>
+      <div style={{ padding: "10px", boxSizing: "border-box" }}>
+        Main
+        {/* <GenreFilter data={data} error={error} isLoading={isLoading} /> */}
+        <GenreFilter />
+        <BooksContainer />
+        <MainFooter />
+      </div>
+    </MainProvider>
   );
 };
 
