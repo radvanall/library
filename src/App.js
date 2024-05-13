5f070ce25f24383ed9e930ccba7a365b3e1d405b
Radvan Alexandru <rip777ton@gmail.com>
2024-05-13 18:48:41 +0300

    Add the book page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Main from "./pages/Main/Main";
+import Book from "./pages/Book/Book";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
+          <Route path=":id" element={<Book />} />
         </Route>
       </Routes>
     </div>
