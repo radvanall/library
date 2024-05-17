4275e51b29e3104da3ac5001a0bafc7b9d946e9f
Radvan Alexandru <rip777ton@gmail.com>
2024-05-17 21:01:01 +0300

    Add tests for Navbar component.

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+
 import Navbar from "./Navbar.js";
 describe("Testing the navbar component", () => {
   beforeEach(() => {
