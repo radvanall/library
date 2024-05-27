fa05073577a4d6988b51b9d81ccb2f69ee810ed0
Radvan Alexandru <rip777ton@gmail.com>
2024-05-27 21:04:46 +0300

    Remove vitals

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />} path="/*" />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />} path="/*" />
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
