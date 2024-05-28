ab7c792577c802c373bc71997066d0b633598dbb
Radvan Alexandru <rip777ton@gmail.com>
2024-05-28 19:17:55 +0300

    minor changes

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import styles from "./User.module.scss";
+import useGet from "../../api/useGet";
 
 const User = () => {
   const { id } = useParams();
+  const { data, error, isLoading } = useGet("/comments/user");
   return <div>User:{id}</div>;
 };
 
