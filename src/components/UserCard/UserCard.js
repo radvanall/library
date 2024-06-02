7f3ffaa475031ba9bb5003b5c249061c8c37b53f
Radvan Alexandru <rip777ton@gmail.com>
2024-06-02 20:54:49 +0300

    Add wrapper

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./UserCard.module.scss";
 const UserCard = ({ user }) => {
-  return <div>login:{user.login}</div>;
+  return <div className={styles.user__card}>login:{user.login}</div>;
 };
 
 export default UserCard;
