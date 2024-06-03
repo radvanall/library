6b6a6f9465854ede7bf38284c13e989f1145af56
Radvan Alexandru <rip777ton@gmail.com>
2024-06-03 21:25:34 +0300

    Add img.

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,7 +1,17 @@
 import React from "react";
 import styles from "./UserCard.module.scss";
+import stat from "../../images/static/book.png";
 const UserCard = ({ user }) => {
-  return <div className={styles.user__card}>login:{user.login}</div>;
+  return (
+    <div className={styles.user__card}>
+      login:{user.login}
+      {user?.avatar ? (
+        <img className={styles.cover} src={user.avatar} alt="#" />
+      ) : (
+        <img className={styles.cover} src={stat} alt="#" />
+      )}
+    </div>
+  );
 };
 
 export default UserCard;
