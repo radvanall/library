349043299c329a94390df052105eca13c63b96fd
Radvan Alexandru <rip777ton@gmail.com>
2024-06-06 15:02:52 +0300

    Add styles to UserComments

---
+++ src/components/UserComments/UserComments.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./UserComments.module.scss";
+
+const UserComments = () => {
+  return <div className={styles.user__comments}>UserComments</div>;
+};
+
+export default UserComments;
