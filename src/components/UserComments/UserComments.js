bc9f76fea269db6d6ea7c16f29428df32142c17d
Radvan Alexandru <rip777ton@gmail.com>
2024-06-12 20:34:31 +0300

    Add usePagination

--- src/components/UserComments/UserComments.js
+++ src/components/UserComments/UserComments.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./UserComments.module.scss";
 import Pagination from "../Pagination/Pagination";
+import usePagination from "../../hooks/usePagination";
 
 const UserComments = ({ id }) => {
   return <div className={styles.user__comments}>UserComments</div>;
