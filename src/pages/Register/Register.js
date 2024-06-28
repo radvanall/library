a23ce820607de2deda9c3b6143e6da90172c3f7f
Radvan Alexandru <rip777ton@gmail.com>
2024-06-28 21:43:52 +0300

    Add the image picker to the form. Add form validation and handleSubmit. Add usePost and implement the post request

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,25 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Register.module.scss";
+import ImageInput from "../../components/ImageInput/ImageInput";
+import usePost from "../../api/usePost";
 
 const Register = () => {
-  let handleSubmit = (event) => {
+  const [value, setValue] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [error, setError] = useState(null);
+  const {
+    postData,
+    setMessage,
+    setError: setBackEndError,
+    message,
+    error: backEndError,
+  } = usePost("/users");
+  const [formValues, setFormValues] = useState({
+    login: "",
+    password: "",
+    repeatPassword: "",
+  });
+  let handleSubmit = async (event) => {
     event.preventDefault();
-
-    let formData = new FormData(event.target);
-    console.log(formData);
+    console.log(formValues);
+    if (
+      formValues.login.length < 6 ||
+      formValues.login.length > 15 ||
+      formValues.password.length < 6 ||
+      formValues.password.length > 15
+    ) {
+      setError("The fields should have between 6 and 15 characters.");
+      return;
+    }
+    if (formValues.password !== formValues.repeatPassword) {
+      setError("The password and repeat password should match.");
+      return;
+    }
+    setError(null);
+    let formData = new FormData();
+    formData.append("login", formValues.login);
+    formData.append("pass", formData.password);
+    if (value) {
+      formData.append("avatar", value);
+      console.log(value);
+    }
+    await postData(formData);
+  };
+  let handleOnChange = (event) => {
+    setFormValues({ ...formValues, [event.target.name]: event.target.value });
   };
   return (
     <div className={styles.form__page}>
       <form onSubmit={handleSubmit}>
         <h2>Register</h2>
         <label htmlFor="login">Username</label>
-        <input type="text" id="login" />
+        <input
+          type="text"
+          id="login"
+          name="login"
+          value={formValues.login}
+          onChange={handleOnChange}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleOnChange}
+          value={formValues.password}
+        />
         <label htmlFor="repeatPassword">Repeat password</label>
-        <input type="password" name="rpassword" id="repeatPassword" />
+        <input
+          type="password"
+          name="repeatPassword"
+          id="repeatPassword"
+          onChange={handleOnChange}
+          value={formValues.repeatPassword}
+        />
+        <ImageInput
+          setValue={setValue}
+          setSelectedImage={setSelectedImage}
+          id="imgPicker"
+          imgName={value?.name ?? "file not selected"}
+        />
+        {error && <p>{error}</p>}
+        {message && <p>{message}</p>}
         <input type="submit" value="Register" />
       </form>
+      <img src={selectedImage} alt="#" />
     </div>
   );
 };
