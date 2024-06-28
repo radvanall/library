ec636875d1ff5447de6e222af6570803cb85d17f
Radvan Alexandru <rip777ton@gmail.com>
2024-06-28 21:42:19 +0300

    Create a component that is used to pick images.

--- src/components/ImageInput/ImageInput.js
+++ src/components/ImageInput/ImageInput.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./ImageInput.module.scss";
 
-const ImageInput = () => {
-  return <div>ImageInput</div>;
+const ImageInput = ({ setValue, setSelectedImage, id, imgName }) => {
+  let ref = useRef();
+  const handleClick = () => {
+    console.log(ref.current.click());
+    console.log(ref.current.value);
+  };
+  const handleChange = (event) => {
+    console.log(event.target.files[0]);
+    if (event.target.files[0]) {
+      setValue(event.target.files[0]);
+      setSelectedImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
+  return (
+    <div className={styles.image__input}>
+      <input
+        type="file"
+        onChange={handleChange}
+        name=""
+        id={id}
+        accept="image/png, image/jpeg"
+        ref={ref}
+      />
+      <button type="button" onClick={handleClick}>
+        Select image
+      </button>
+      <label htmlFor={id}>{imgName}</label>
+    </div>
+  );
 };
 
 export default ImageInput;
