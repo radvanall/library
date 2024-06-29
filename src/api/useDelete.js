72473b9a3329a237a5bb5ffeba15de679786232d
Radvan Alexandru <rip777ton@gmail.com>
2024-06-29 22:18:12 +0300

    Create the useDelete hook

---
+++ src/api/useDelete.js
@@ -0,0 +1,20 @@
+import api from "./api";
+import { useState } from "react";
+
+const useDelete = (dataUrl) => {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  const deleteData = async (id) => {
+    try {
+      const res = await api.delete(`${dataUrl}/${id}`);
+      console.log(res);
+      setMessage(res.data.message);
+    } catch (err) {
+      console.log(err.response.data.message);
+      setError(err.response.data.message);
+    }
+  };
+  return { deleteData, setMessage, setError, message, error };
+};
+export default useDelete;
