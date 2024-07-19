e50f8a97e2206056f15d92a11e3bba0fa4db2f8a
Radvan Alexandru <rip777ton@gmail.com>
2024-07-19 22:53:40 +0300

    Replaced the defaultChecked with the checked

--- src/components/GenreFilter/GenreFilter.js
+++ src/components/GenreFilter/GenreFilter.js
@@ -6,36 +6,13 @@ import { MainContext } from "../../context/MainContext";
 // const GenreFilter = ({ data, isLoading, error }) => {
 const GenreFilter = () => {
   //   const { data, error, isLoading } = useGet("genres/book-count");
+
   const { genres, isLoading, error, params, setParams, handleGenreChange } =
     useContext(MainContext);
-  // const handleGenreChange = (e) => {
-  //   console.log(e.target.checked);
-  //   // e.target.checked = !e.target.checked;
-  //   console.log(e.target.name);
-  //   console.log(e.target.id);
-  //   console.log(e.target);
-  //   let newIgnoredGenres = JSON.parse(params.ignoredGenres);
-  //   console.log(newIgnoredGenres);
-
-  //   if (e.target.checked) {
-  //     const index = newIgnoredGenres.indexOf(parseInt(e.target.id));
-  //     console.log("index=", index);
-  //     if (index >= 0) {
-  //       newIgnoredGenres.splice(index, 1);
-  //     }
-  //     console.log("newIgnoredGenres slice=", newIgnoredGenres);
-  //   } else {
-  //     newIgnoredGenres.push(parseInt(e.target.id));
-  //   }
-  //   console.log(newIgnoredGenres);
-  //   setParams({
-  //     ...params,
-  //     page: 1,
-  //     ignoredGenres: JSON.stringify(newIgnoredGenres),
-  //   });
-  // };
-
   console.log("data", genres);
+  const ignoredGenres = JSON.parse(params.ignoredGenres).map((genre) =>
+    parseInt(genre)
+  );
   return (
     <div className={styles.genre__filter__container}>
       {isLoading && <p>Is loading</p>}
@@ -48,12 +25,28 @@ const GenreFilter = () => {
               label={genre.genre}
               key={genre.id}
               id={genre.id}
-              defaultChecked={
+              // defaultChecked={
+              //   params?.requiredGenre
+              //     ? parseInt(params.requiredGenre) === parseInt(genre.id)
+              //       ? true
+              //       : false
+              //     : true
+              // }
+              // checked={
+              //   params?.requiredGenre
+              //     ? parseInt(params.requiredGenre) === parseInt(genre.id)
+              //       ? true
+              //       : false
+              //     : true
+              // }
+              checked={
                 params?.requiredGenre
                   ? parseInt(params.requiredGenre) === parseInt(genre.id)
                     ? true
                     : false
-                  : true
+                  : ignoredGenres.indexOf(parseInt(genre.id)) === -1
+                  ? true
+                  : false
               }
               quantity={genre.nrOfBooks}
               onChange={handleGenreChange}
