e38c726f22756a1890bba6d4e5bd93b00a6a3a67
Radvan Alexandru <rip777ton@gmail.com>
2024-07-22 22:43:31 +0300

    Create a hook used for login.

---
+++ src/api/useLogin.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import api from "./api";
+import { useAuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const useLogin = (dataUrl) => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { setUserData } = useAuthContext();
+  const navigate = useNavigate();
+  const getUser = async (data) => {
+    setIsLoading(true);
+    try {
+      console.log({ data });
+      const response = await api.get(dataUrl, { params: data });
+      console.log({ response });
+      setUserData(response.data);
+      navigate(`/user/${response.data.user.id}`);
+    } catch (err) {
+      setError(err.response.data.message);
+      console.log(err.message);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+        setError(null);
+      }, [3000]);
+    }
+  };
+  return { getUser, error, isLoading };
+};
+export default useLogin;
