37cf97cbd248a677ddd3fa234670f8cf12446e64
Radvan Alexandru <rip777ton@gmail.com>
2024-07-25 17:07:50 +0300

    Add UserUpdateBlock and refetch to the component.

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./UserCard.module.scss";
 import stat from "../../images/static/avatar.jpg";
-const UserCard = ({ user }) => {
+import roles from "../../roles";
+import UserUpdateBlock from "../UserUpdateBlock/UserUpdateBlock";
+const UserCard = ({ user, refetch }) => {
+  console.log("av=", user.avatar);
+  const avatar = user.avatar
+    ? "/images/users/" + user.avatar.split("/").pop()
+    : stat;
+
+  console.log("av2=", avatar);
   return (
     <div className={styles.user__card}>
-      {user?.avatar ? (
-        <img className={styles.cover} src={user.avatar} alt="#" />
-      ) : (
-        <img className={styles.cover} src={stat} alt="#" />
-      )}
-      {user.login}
+      <img className={styles.cover} src={avatar} alt="#" />
+      <p> {user.login}</p>
+      <p>{roles[user.role]}</p>
+      <UserUpdateBlock avatar={avatar} user={user} refetch={refetch} />
     </div>
   );
 };
