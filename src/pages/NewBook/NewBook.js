9ddbd3d31c4ad4b50818719e920e9b09d642aad2
Radvan Alexandru <rip777ton@gmail.com>
2024-07-27 15:02:15 +0300

    Add usePost on the newBook page.

--- src/pages/NewBook/NewBook.js
+++ src/pages/NewBook/NewBook.js
@@ -1,10 +1,14 @@
 import React from "react";
 import BookForm from "../../components/BookForm/BookForm";
 import styles from "./NewBook.module.scss";
+import usePost from "../../api/usePost";
 const NewBook = () => {
+  const { postData, setMessage, setError, message, error } = usePost("books");
   return (
     <div className={styles.new__book}>
-      <BookForm></BookForm>
+      <BookForm submitData={postData} isCreateMode={true} />
+      {message && message}
+      {error && error}
     </div>
   );
 };
