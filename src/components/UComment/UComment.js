4871375c82a5454dd91da8fd40a096d44224843c
Radvan Alexandru <rip777ton@gmail.com>
2024-07-27 15:04:15 +0300

    Add image to the UComment component.

--- src/components/UComment/UComment.js
+++ src/components/UComment/UComment.js
@@ -2,7 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import imgHolder from "../../images/static/book.png";
 import styles from "./UComment.module.scss";
+import stat from "../../images/static/book.png";
 const UComment = ({ comment, activateModal, removeComment }) => {
+  const cover = comment?.cover
+    ? "/images/books/" + comment.cover.split("/").pop()
+    : stat;
   return (
     <div className={styles.comment__container}>
       {comment.bookId ? (
@@ -16,11 +20,12 @@ const UComment = ({ comment, activateModal, removeComment }) => {
         <p>inactive title</p>
       )}
       <div className={styles.comment__body}>
-        {comment?.cover ? (
+        <img className={styles.cover} src={cover} alt="#" />
+        {/* {comment?.cover ? (
           <img className={styles.cover} src={comment.cover} alt="#" />
         ) : (
           <img className={styles.cover} src={imgHolder} alt="#" />
-        )}
+        )} */}
         <div className={styles.button__container}>
           <button onClick={() => activateModal(comment)}>Edit</button>
           <button onClick={() => removeComment(comment.commentId)}>
