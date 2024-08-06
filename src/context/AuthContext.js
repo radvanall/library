54776fc49fb13f65e7328a5280bf0896a4db496c
Radvan Alexandru <rip777ton@gmail.com>
2024-08-06 03:28:17 +0300

    Change authContext.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,25 +2,36 @@ import { useContext, createContext, useState } from "react";
 
 const AuthContext = createContext({});
 
+const setInitialUserData = () => {
+  const userData = window.localStorage.getItem("userData");
+  if (userData) {
+    return JSON.parse(userData);
+  } else
+    return {
+      id: null,
+      login: "",
+      avatar: null,
+      role: null,
+      accessToken: "",
+    };
+};
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    id: null,
-    login: "",
-    avatar: null,
-    role: null,
-    accessToken: "",
-  });
+  const [user, setUser] = useState(setInitialUserData);
   const setUserData = (data) => {
-    setUser({
+    let userData = {
       ...data.user,
       accessToken: data?.accessToken,
-    });
+    };
+    setUser(userData);
+    window.localStorage.setItem("userData", JSON.stringify(userData));
   };
   const resetAccessToken = (data) => {
-    setUser((prev) => ({
-      ...prev,
+    let userData = {
+      ...user,
       accessToken: data,
-    }));
+    };
+    setUser(userData);
+    window.localStorage.setItem("userData", JSON.stringify(userData));
   };
   const deleteUserData = () => {
     setUser({
@@ -30,6 +41,7 @@ const AuthProvider = ({ children }) => {
       role: null,
       accessToken: "",
     });
+    window.localStorage.removeItem("userData");
   };
   return (
     <AuthContext.Provider
