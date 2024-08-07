5a4124f8190c8b0d1102ca1926520eb67f822d0d
Radvan Alexandru <rip777ton@gmail.com>
2024-08-07 23:23:38 +0300

    Add links.

---
+++ src/api/links.js
@@ -0,0 +1,43 @@
+const links = {
+  1111: [
+    {
+      address: "/",
+      name: "Books",
+    },
+    {
+      address: "/login",
+      name: "Login",
+    },
+    {
+      address: "/register",
+      name: "Register",
+    },
+  ],
+  2001: [
+    {
+      address: "/",
+      name: "Books",
+    },
+  ],
+  1984: [
+    {
+      address: "/",
+      name: "Books",
+    },
+    {
+      address: "/users",
+      name: "Users",
+    },
+  ],
+  5150: [
+    {
+      address: "/",
+      name: "Books",
+    },
+    {
+      address: "/users",
+      name: "Users",
+    },
+  ],
+};
+export { links };
